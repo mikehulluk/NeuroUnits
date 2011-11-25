997cb9c303787932dea9624e41018967ef188e2f
Mike Hull (Bristol) <mikehulluk@googlemail.com>
2011-11-25 20:02:04 +0000

    Missing Files from previous commit

--- src/python_ply/units_data.py
+++ src/python_ply/units_data.py
@@ -1,9 +1,14 @@
 from units_core import Unit
 
 multipliers = (
-       ( 'giga','G', 9) ),
-       ( 'mega','M', 6) ),
-       ( 'kilo','k', 3) ),
+       ( 'giga','G', 9) ,
+       ( 'mega','M', 6) ,
+       ( 'kilo','k', 3) ,
+       ( 'centi','c', -2) ,
+       ( 'milli','m', -3) ,
+       ( 'micro','u', -6) ,
+       ( 'nano','n', -9) ,
+       ( 'pico','p', -12) ,
         )
 
 units = (
@@ -18,10 +23,16 @@ units = (
        ( 'volt','V', Unit(meter=2, kilogram=1,second=-3,ampere=-1) ),
        ( 'siemen','S', Unit(meter=-2, kilogram=-1,second=3,ampere=2) ),
        ( 'farad','F', Unit(meter=-2, kilogram=-1,second=4,ampere=2) ),
-
+       ( 'ohm','Ohm', Unit(meter=2, kilogram=1,second=-3,ampere=-2) ),
+       ( 'coulomb','C', Unit(second=1,ampere=1) ),
        )
 
 special_unit_abbrs = ( 
         ( 'm', Unit(meter=1)  ),
+        ( 'cm', Unit(meter=1, powerTen=-2)  ),
+        ( 'mm', Unit(meter=1, powerTen=-3)  ),
+        ( 'um', Unit(meter=1, powerTen=-6)  ),
+        ( 'nm', Unit(meter=1, powerTen=-9)  ),
+        ( 'pm', Unit(meter=1, powerTen=-12)  ),
 
             )
