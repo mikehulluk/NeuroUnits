80955deadd12eb847626e5095ac9e28b72e22bd2
Mike Hull (New Laptop) <mikehulluk@googlemail.com>
2013-02-15 07:23:06 +0000

    Extraction works

--- test1.sh
+++ test1.sh
@@ -1,4 +1,4 @@
 
-./bin/neurounits --extract-level=L1 --extract-to='L1.txt' --validate src/test_data/valid_l1.nuts
-./bin/neurounits --extract-level=L2 --extract-to='L2.txt' --validate src/test_data/valid_l1.nuts
-./bin/neurounits --extract-level=L3 --extract-to='L3.txt' --validate src/test_data/valid_l1.nuts
+./bin/neurounits --extract --extract-level=L1 --extract-to='L1.txt'  src/test_data/valid_l1.nuts
+./bin/neurounits --extract --extract-level=L2 --extract-to='L2.txt'  src/test_data/valid_l1.nuts
+./bin/neurounits --extract --extract-level=L3 --extract-to='L3.txt'  src/test_data/valid_l1.nuts
