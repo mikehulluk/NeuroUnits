20bbc0b80623729a53813067bf9482c825e30fb5
Michael Hull <mikehulluk@googlemail.com>
2013-07-05 17:25:20 +0100

    ...

--- indev/fixed_point_mapping/test1.py
+++ indev/fixed_point_mapping/test1.py
@@ -359,7 +359,7 @@ print 'Looking at mappings:'
 print '===================='
 
 
-nbits = 30
+nbits = 17
 annotations = ASTDataAnnotator( comp, annotations_in = var_annots)
 CalculateInternalStoragePerNode(annotations=annotations, nbits=nbits).visit(comp)
 
