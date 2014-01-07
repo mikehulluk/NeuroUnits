fa59730fc863fa7abb852fb743ddf78a27727d34
Michael Hull <mikehulluk@googlemail.com>
2014-01-07 10:54:39 +0000

    ...

---
+++ indev/fixed_point_mapping/plot_raster.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib
+from matplotlib.patches import Circle, Wedge, Polygon
+from matplotlib.collections import PatchCollection
+import matplotlib.pyplot as plt
+
+
+import pylab
+import numpy as np
+from hdfjive import HDF5SimulationResultFile, RasterGroup, RasterSubgroup
+results = HDF5SimulationResultFile("/local/scratch/mh735/neuronits.results-Seq-float.hdf")
+
+import pickle
+with open('mh_reduced_connectome.pickle') as f:
+    pop_sizes, connections, pop_breakdowns, cell_positions = pickle.load(f)
+
+
+pop_info = {
+    'RB' : ('yellow', 7.),
+    'dla': ('red',    6.),
+    'dlc': ('red',    5.),
+    
+    'dIN': ('brown',  4.),
+    'aIN': ('red',    3.),
+    'cIN': ('cyan',   2.),
+    'MN' : ('green',  1.),
+}
+
+
+
+
+plotting_info = {
+    
+}
+
+
+
+f = pylab.figure()
+ax = f.add_subplot(111)
+
+for (is_lhs,cell_type), (saved_pop_name, start_index, ncells) in pop_breakdowns.items():
+    print 'Plotting: ', cell_type
+    pop_color, pop_y_index = pop_info[cell_type]
+    
+    pop_y_pos = pop_y_index * (-1. if is_lhs else 1.) * 100.
+    pop_x_poses = cell_positions[saved_pop_name][start_index:start_index+ncells]
+    r = 10
+    
+    patches = []
+    for x1 in pop_x_poses:
+        circle = Circle((x1,pop_y_pos), r)
+        patches.append(circle)
+
+
+    cmap = matplotlib.colors.ListedColormap(colors=['white', pop_color, pop_color], name='from_list')
+    #cmap = matplotlib.cm.ScalarMappable(norm= lambda x: (x-(-70,))/(50+70), cmap=cmap)
+
+    #cmap = matplotlib.cm.jet
+
+    
+    colors = np.random.random(ncells)
+    colors = colors * colors
+    #p = PatchCollection(patches, cmap=cmap, alpha=0.4)
+    p = PatchCollection(patches, cmap=cmap, alpha=1.0)
+    
+    p.set_array(colors)
+    ax.add_collection(p)
+
+
+    #~ # Store Key: -> (pts, color, start_index, stop_index)
+    #~ plotting_info[ (is_lhs,cell_type) ] = (p, pop_color, start_index, start_index+ncells)
+    #~ plt.colorbar(p)
+
+
+
+ax.set_ylim(-1000,1000)
+ax.set_xlim(400,2000)
+
+
+
+
+pylab.show()
+
+
+
+print 'Done'
+
+
+
+
+
---
+++ indev/fixed_point_mapping/test_plotting_circles.py
@@ -0,0 +1,27 @@
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Circle
+import numpy as np
+
+# (modified from one of the matplotlib gallery examples)
+resolution = 50 # the number of vertices
+N = 100
+x       = np.random.random(N)
+y       = np.random.random(N)
+radii   = 0.1*np.random.random(N)
+patches = []
+for x1,y1,r in zip(x, y, radii):
+    circle = Circle((x1,y1), r)
+    patches.append(circle)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+colors = 100*np.random.random(N)
+p = PatchCollection(patches, cmap=matplotlib.cm.jet, alpha=0.4)
+p.set_array(colors)
+ax.add_collection(p)
+plt.colorbar(p)
+
+plt.show()
