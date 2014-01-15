215f8b10fdd7d7686de49a05c0d62bc18b3f3e80
Michael Hull <mikehulluk@googlemail.com>
2014-01-15 09:50:43 +0000

    Cleaning up

--- test_extract.sh
+++
@@ -1,3 +0,0 @@
-/home/michael/hw_to_come/NeuroUnits/bin/neurounits --extract --extract-level=L1 --extract-to='test1.txt' /home/michael/hw_to_come/NeuroUnits/src/test_data/*.nuts 
-/home/michael/hw_to_come/NeuroUnits/bin/neurounits --extract --extract-level=L2 --extract-to='test2.txt' /home/michael/hw_to_come/NeuroUnits/src/test_data/*.nuts 
-
